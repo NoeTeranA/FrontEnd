107fd4d3bb96ac458dcb216a8a6a4beb77ea0c52
NoeTeranA <noeteran190489@gmail.com>
2022-03-17 23:40:34 -0600

    Avance pokedex con juanito

--- PRACTICAS JS/pokedex.js
+++ PRACTICAS JS/pokedex.js
@@ -2,28 +2,36 @@ const fetchPokemon = () => {
     const pokeNameInput = document.getElementById("pokeName");
     let pokeName = pokeNameInput.value;
     pokeName = pokeName.toLowerCase();
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
-    fetch(url).then((res) => {
-        if (res.status != "200") {
-            console.log(res);
-            pokeImage("./pikachu-sad.gif")
+    const pokeapi = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(pokeapi).then((objetoPokeapi) => {
+        if (objetoPokeapi.status != "200") {
+            console.log(objetoPokeapi);
+            pokeImage("./Assets/pikachu-sad.gif")
         }
         else {
-            return res.json();
+            return objetoPokeapi .json();
         }
-    }).then((data) => {
-        if (data) {
-            console.log(data);
-            let pokeImg = data.sprites.front_default;
+    }).then((datos) => {
+        if (datos) {
+            console.log(datos);
+            let pokeImg = datos.sprites.front_default;
             pokeImage(pokeImg);
+            let pokeNombre = datos.name;
+            nombrePokemon(pokeNombre.toUpperCase())
             console.log(pokeImg);
         }
     });
 }
 
-const pokeImage = (url) => {
+//Esto sirve para mandar los datos del api a los ids del html
+
+const pokeImage = (pokeapi) => {
     const pokePhoto = document.getElementById("pokeImg");
-    pokePhoto.src = url;
+    pokePhoto.src = pokeapi;
+}
+const nombrePokemon = (pokeapi) => {
+    const nombre = document.getElementById("nombrePokemon");
+    nombre.innerText = pokeapi;
 }
 
 
